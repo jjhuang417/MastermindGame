7db3292b5b06dfbacfa69733eed21e4448a08b1b
Jesse Huang <jjhuang417@gmail.com>
2022-04-14 20:33:33 -0700

    Cleaning up code consistency.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,8 +10,8 @@ const App = () => {
 
   // State
   const [sequence, setSequence] = useState('');
-  const [guesses, SetGuess] = useState(10);
-  const [playerInput, setInput] = useState([]);
+  const [guess, SetGuess] = useState(10);
+  const [playerInput, setPlayerInput] = useState([]);
 
   // Get sequence and save as state
   useEffect(() => {
@@ -27,14 +27,14 @@ const App = () => {
   // F(n) to add number
   const addNum = (num) => {
     if (playerInput.length < 4) {
-      setInput([...playerInput, num]);
+      setPlayerInput([...playerInput, num]);
     }
   }
 
   // F(n) to remove a digit
   const deleteNum = () => {
     let sliced = playerInput.slice(0, playerInput.length - 1);
-    setInput(sliced);
+    setPlayerInput(sliced);
   }
 
   // F(n) to submit playerInput
@@ -46,17 +46,17 @@ const App = () => {
         <h1 className='title'>Mastermind Game 🔍</h1>
       </div>
       <div className='guessCounterDiv'>
-        <h2>{guesses} {guesses > 1 ? 'Tries' : 'Try'} Left</h2>
+        <h2>{guess} {guess > 1 ? 'Tries' : 'Try'} Left</h2>
       </div>
       <Numpad
         playerInput={playerInput}
-        setInput={setInput}
+        setPlayerInput={setPlayerInput}
         addNum={addNum}
         deleteNum={deleteNum}
       />
       <GameBoard
         playerInput={playerInput}
-        setInput={setInput}
+        setPlayerInput={setPlayerInput}
       />
       <InputHistory />
     </div>
