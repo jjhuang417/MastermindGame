aebf2d41d7b5b58a64997d546c5b76ad93619edf
Jesse Huang <jjhuang417@gmail.com>
2022-04-15 21:15:54 -0700

    Adjusted visual styling for how many numbers.

--- client/src/GameBoard.jsx
+++ client/src/GameBoard.jsx
@@ -1,26 +1,24 @@
 import React from 'react';
 
 const GameBoard = (props) => {
+  let [input1, input2, input3, input4] = props.playerInput;
 
   return (
     <div className='gameBoard'>
-      {props.playerInput.map((int, idx)  => (
-        <h1
-          className='playerInput'
-          key={idx}>{int}
-        </h1>
-      ))}
+      <div className='underline'>
+        {input1 ? <h1>{input1}</h1> : null}
+      </div>
+      <div className='underline'>
+        {input2 ? <h1>{input2}</h1> : null}
+      </div>
+      <div className='underline'>
+        {input3 ? <h1>{input3}</h1> : null}
+      </div>
+      <div className='underline'>
+        {input4 ? <h1>{input4}</h1> : null}
+      </div>
     </div>
   );
 }
 
 export default GameBoard;
-
-{/* <div className='gameBoard'>
-{props.playerInput.map((int, idx)  => (
-  <h1
-    className='playerInput'
-    key={idx}>{int}
-  </h1>
-))}
-</div> */}
