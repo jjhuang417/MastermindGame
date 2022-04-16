88c59e8b74483fc9c725d06d8dfa2135df71ecb9
Jesse Huang <jjhuang417@gmail.com>
2022-04-16 15:33:56 -0700

    added additional button styling.

--- client/src/Numpad.jsx
+++ client/src/Numpad.jsx
@@ -30,19 +30,19 @@ const Numpad = (props) => {
   return (
     <div name='numPadWrap' className='numPadWrap'>
       <div className='functionButton'>
-        <button onClick={handleAdd} type='button' value='0'>0</button>
-        <button  onClick={handleAdd}  type='button' value='1'>1</button>
-        <button onClick={handleAdd}  type='button' value='2'>2</button>
-        <button onClick={handleAdd}  type='button' value='3'>3</button>
-        <button onClick={handleAdd}  type='button' value='4'>4</button>
-        <button onClick={handleAdd}  type='button' value='5'>5</button>
-        <button onClick={handleAdd}  type='button' value='6'>6</button>
-        <button onClick={handleAdd}  type='button' value='7'>7</button>
+        <button className='button-22' onClick={handleAdd} type='button' value='0'>0</button>
+        <button className='button-22' onClick={handleAdd} type='button' value='1'>1</button>
+        <button className='button-22' onClick={handleAdd} type='button' value='2'>2</button>
+        <button className='button-22' onClick={handleAdd} type='button' value='3'>3</button>
+        <button className='button-22' onClick={handleAdd} type='button' value='4'>4</button>
+        <button className='button-22' onClick={handleAdd} type='button' value='5'>5</button>
+        <button className='button-22' onClick={handleAdd} type='button' value='6'>6</button>
+        <button className='button-22' onClick={handleAdd} type='button' value='7'>7</button>
       </div>
       <div className='functionButton'>
-        <button onClick={handleDelete}>Delete</button>
-        <button onClick={handleWipe}>Reset Input</button>
-        <button onClick={handleSubmit}>Submit</button>
+        <button className='button-25' onClick={handleDelete}>Delete</button>
+        <button className='button-25' onClick={handleWipe}>Reset Input</button>
+        <button className='button-25' onClick={handleSubmit}>Submit</button>
       </div>
     </div>
   )
