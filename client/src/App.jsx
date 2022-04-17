ffb315763bcd1fd338df73be332e326f25f41601
Jesse Huang <jjhuang417@gmail.com>
2022-04-17 15:00:50 -0700

    Refactored page initialization to send code to the front end.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,101 +1,107 @@
 // Framework & Packages
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 // Componenet import
-import Numpad from './Numpad.jsx';
-import GameBoard from './GameBoard.jsx';
-import InputHistory from './InputHistory.jsx';
+import Numpad from "./Numpad.jsx";
+import GameBoard from "./GameBoard.jsx";
+import InputHistory from "./InputHistory.jsx";
 
 const App = () => {
-
   // State
   const [win, setWin] = useState(false);
-  const [sequence, setSequence] = useState('');
+  const [sequence, setSequence] = useState("");
   const [guess, setGuess] = useState(10);
   const [playerInput, setPlayerInput] = useState([]);
   const [history, setHistory] = useState([]);
 
   // Win & Lose conditions
   if (win) {
-    alert('CONGRATS!!! YOU DID IT!');
-  } else if ((!win && guess < 1)) {
-    axios.get('/answer')
-    .then ((res) => {
-     let answer = res.data;
-      alert(`YOU DIED...Should've guess ${answer}`);
-    })
-    .catch ((err) => {
-      console.log(err);
-    })
+    alert("CONGRATS!!! YOU DID IT!");
+  } else if (!win && guess < 1) {
+    axios
+      .get("/answer")
+      .then((res) => {
+        let answer = res.data;
+        alert(`YOU DIED...Should've guess ${answer}`);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 
   // Get sequence and save as state
   useEffect(() => {
-    axios.get('/initialize')
-    .then((res) => {
-      console.log('Game Start');
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+    axios
+      .get("/initialize")
+      .then((res) => {
+        console.log("Game Start");
+        setSequence(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }, []);
+
+  console.log(sequence);
 
   // F(n) to add number
   const addNum = (num) => {
     if (playerInput.length < 4) {
       setPlayerInput([...playerInput, num]);
     }
-  }
+  };
 
   // F(n) to remove a digit
   const deleteNum = () => {
     let sliced = playerInput.slice(0, playerInput.length - 1);
     setPlayerInput(sliced);
-  }
+  };
 
   // F(n) to wipe out user input
   const wipeNum = () => {
     setPlayerInput([]);
-  }
+  };
 
   // F(n) to submit playerInput
   const submitGuess = () => {
     if (playerInput.length === 4) {
-      axios.get('/submit', {
-        params: {
-          input: playerInput
-        }
-      })
-      .then((response) => {
-        setHistory([...history,
-          {
-            num: playerInput,
-            feedback: response.data
+      axios
+        .get("/submit", {
+          params: {
+            input: playerInput,
+          },
+        })
+        .then((response) => {
+          setHistory([
+            ...history,
+            {
+              num: playerInput,
+              feedback: response.data,
+            },
+          ]);
+          setGuess(guess - 1);
+          let winNum = 0;
+          for (let i = 0; i < response.data.length; i++) {
+            if (response.data[i] === "correct") {
+              winNum += 1;
+            }
+            if (winNum === 4) {
+              setWin(true);
+            }
           }
-        ]);
-        setGuess(guess - 1);
-        let winNum = 0;
-        for (let i = 0; i < response.data.length; i++) {
-          if (response.data[i] === 'correct') {
-            winNum += 1;
-          }
-          if (winNum === 4) {
-            setWin(true);
-          };
-        }
-        setPlayerInput([]);
-      })
-      .catch((error) => {
-        console.log(error);
-      })
+          setPlayerInput([]);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     }
-  }
+  };
 
   return (
-    <div className='highestDiv'>
-      <div className='titleWrap'>
-        <h1 className='title'>Mastermind Game 🔍</h1>
+    <div className="highestDiv">
+      <div className="titleWrap">
+        <h1 className="title">Mastermind Game 🔍</h1>
       </div>
       <Numpad
         addNum={addNum}
@@ -105,21 +111,18 @@ const App = () => {
         win={win}
         setWin={setWin}
       />
-      <div className='centerHistory'>
-        <GameBoard
-          playerInput={playerInput}
-        />
+      <div className="centerHistory">
+        <GameBoard playerInput={playerInput} />
       </div>
-      <div className='centerHistory'>
+      <div className="centerHistory">
         <InputHistory
           history={history}
           playerInput={playerInput}
           guess={guess}
         />
       </div>
-
     </div>
-  )
+  );
 };
 
 export default App;
--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 // Frameworks & Packages
-const express = require('express');
-const axios = require('axios');
+const express = require("express");
+const axios = require("axios");
 
 // App & Port
 const app = express();
 const port = 3000;
 
 // Serving static files
-app.use(express.static('client/dist'));
+app.use(express.static("client/dist"));
 app.use(express.json());
 
 // Connecting on the port
@@ -15,66 +15,93 @@ app.listen(port, (req, res) => {
   console.log(`Listening on port ${port}`);
 });
 
+// Endpoint for page initial render
+app.get("/initialize", (req, res) => {
+  let intURL =
+    "https://www.random.org/integers/?num=4&min=0&max=7&col=1&base=10&format=plain&rnd=new";
+  axios
+    .get(intURL)
+    .then((response) => {
+      let code = response.data;
+      code = code.split("\n");
+      code = 
+      res.status(200).send(code);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
 // Answer sequence from the random API
-let answerSequence;
+// let answerSequence;
 
 // API Call to get sequence
-const getAnswer = async () => {
-  let intURL = 'https://www.random.org/integers/?num=4&min=0&max=7&col=1&base=10&format=plain&rnd=new';
-  let result = await axios.get(intURL);
-  answerSequence = result.data.split('\n');
-  answerSequence.pop();
-  // console.log('answer on start up:', answerSequence);
-};
+// const getAnswer = async () => {
+//   let intURL =
+//     "https://www.random.org/integers/?num=4&min=0&max=7&col=1&base=10&format=plain&rnd=new";
+//   let result = await axios.get(intURL);
+//   answerSequence = result.data.split("\n");
+//   answerSequence.pop();
+//   // console.log('answer on start up:', answerSequence);
+// };
 
-getAnswer();
+// const getCode = async () => {
+//   let intURL =
+//     "https://www.random.org/integers/?num=4&min=0&max=7&col=1&base=10&format=plain&rnd=new";
+//   let rawCode = await axios.get(intURL);
+//   let code = rawCode.data.split("\n");
+//   code.pop();
+//   console.log(code);
+//   return code;
+// };
+
+// getAnswer();
 
 // Function for checking game logic
-const checkGuess = (answer, guess) => {
-  let feedback = [];
-  for (let i = 0; i < guess.length; i++) { // we want to first find all the correct nums and change them to -1s to eliminate them.
-    if (guess[i] === answer[i]) {
-      feedback.push('correct'); // add 'correct' into the feedback arr
-      guess[i] = -1; // change the element to -1.
-      answer[i] = -1; // change the element to -1.
-    }
-  }
-  for (let j = 0; j < answer.length; j++) { // Need to a loop to check the rest of the answers
-    let include = false; // include a true or false flag to check to see if it's a partial number
-    if (answer[j] === -1) continue; // whenever we hit a -1 we skip since we already checked the num
-    for (let k = 0; k < guess.length; k++) { // use a second loop to iterate over the guess
-      if (guess[k] === -1) { // make sure we skip -1s in the guess
-        continue;
-      } else if (answer[j] === guess[k]) { // if an elemnt in the guess equals to one in the answer, this means we hit a partial
-        feedback.push('partial'); // push it into the feedback arr
-        guess[k] = -1; // turn the elemnt in guess into a -1 to make sure we dont double count. The answer will only be checked once
-        include = true;
-        break;
-      }
-    }
-    if (!include) feedback.push('wrong'); // push wrong into the array if none of the blocks above ran.
-  }
-  return feedback.sort(); // sort the arr to make sure the correct answers come first.
-}
-
-// Endpoint for page initial render
-app.get('/initialize', (req, res) => {
-  getAnswer();
-  res.status(200).send();
-});
+// const checkGuess = (answer, guess) => {
+//   let feedback = [];
+//   for (let i = 0; i < guess.length; i++) {
+//     // we want to first find all the correct nums and change them to -1s to eliminate them.
+//     if (guess[i] === answer[i]) {
+//       feedback.push("correct"); // add 'correct' into the feedback arr
+//       guess[i] = -1; // change the element to -1.
+//       answer[i] = -1; // change the element to -1.
+//     }
+//   }
+//   for (let j = 0; j < answer.length; j++) {
+//     // Need to a loop to check the rest of the answers
+//     let include = false; // include a true or false flag to check to see if it's a partial number
+//     if (answer[j] === -1) continue; // whenever we hit a -1 we skip since we already checked the num
+//     for (let k = 0; k < guess.length; k++) {
+//       // use a second loop to iterate over the guess
+//       if (guess[k] === -1) {
+//         // make sure we skip -1s in the guess
+//         continue;
+//       } else if (answer[j] === guess[k]) {
+//         // if an elemnt in the guess equals to one in the answer, this means we hit a partial
+//         feedback.push("partial"); // push it into the feedback arr
+//         guess[k] = -1; // turn the elemnt in guess into a -1 to make sure we dont double count. The answer will only be checked once
+//         include = true;
+//         break;
+//       }
+//     }
+//     if (!include) feedback.push("wrong"); // push wrong into the array if none of the blocks above ran.
+//   }
+//   return feedback.sort(); // sort the arr to make sure the correct answers come first.
+// };
 
 // Route for guess checking
-app.get('/submit', (req, res) => {
-  let copyAnswer = answerSequence.slice(0);
-  let copyInput = req.query.input.slice(0);
-  // console.log('answer: ', copyAnswer);
-  // console.log('player: ', copyInput);
-  let results = checkGuess(copyAnswer, copyInput);
-  res.status(200).send(results);
-});
+// app.get("/submit", (req, res) => {
+//   let copyAnswer = answerSequence.slice(0);
+//   let copyInput = req.query.input.slice(0);
+//   // console.log('answer: ', copyAnswer);
+//   // console.log('player: ', copyInput);
+//   let results = checkGuess(copyAnswer, copyInput);
+//   res.status(200).send(results);
+// });
 
 // Route for grabbing the right answer once the player loses
-app.get('/answer', (req, res) => {
-  const joinedAnswer = answerSequence.join('');
-  res.status(200).send(joinedAnswer);
-});
+// app.get("/answer", (req, res) => {
+//   const joinedAnswer = answerSequence.join("");
+//   res.status(200).send(joinedAnswer);
+// });
